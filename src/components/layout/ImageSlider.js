d194e0401643ec61e21ad633776f1b2dda6f8d7f
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-03-04 14:33:24 +0200

    imagesliderImprove

--- src/components/layout/ImageSlider.js
+++ src/components/layout/ImageSlider.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import download12 from '../card/k.jpg'
-import {Carousel} from 'react-responsive-carousel';
+import React from "react";
+import download12 from "../card/k.jpg";
+import download13 from "../card/k2.jpg";
+import download14 from "../card/k1.jpg";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 
 const ImageSlider = () => {
   return (
-    <Carousel showThumbs={false} infiniteLoop={true} width={'100%'} autoPlay={true}>
-                <div>
-                    <img src={download12} />
-                    
-                </div>
-                <div>
-                    <img src={download12} />
-                    
-                </div>
-                <div>
-                    <img src={download12} />
-                    
-                </div>
-                
-            </Carousel>
-  )
-}
+    <Carousel
+      showThumbs={false}
+      infiniteLoop={true}
+      width={"100%"}
+      height={"100%"}
+      autoPlay={true}
+      useKeyboardArrows={true}
+      stopOnHover={true}
+      dynamicHeight={true}
+      emulateTouch={true}
+    >
+      <div>
+        <img src={download12} />
+      </div>
+      <div>
+        <img src={download13} />
+      </div>
+      <div>
+        <img src={download14} />
+      </div>
+    </Carousel>
+  );
+};
 
-export default ImageSlider
+export default ImageSlider;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import ImageSlider from '../layout/ImageSlider'
-import Cards from '../card/Cards'
-import Navbar from '../layout/NavBar'
-import NavImg from '../../download.png'
-import Footer from '../layout/Footer'
-import Specs from '../layout/Specs'
-import ScrollButton from '../layout/ScrollButton'
-
-const Home = () => {
-
+import React, { Component } from "react";
+import ImageSlider from "../layout/ImageSlider";
+import Cards from "../card/Cards";
+import Navbar from "../layout/NavBar";
+import NavImg from "../../download.png";
+import Footer from "../layout/Footer";
+import Specs from "../layout/Specs";
+import ScrollButton from "../layout/ScrollButton";
+class Home extends Component {
+  render() {
     return (
-        <div>
-
-            <Navbar bg={NavImg} />
-            <ImageSlider scrollStepInPx="1" delayInMs="16.66"/>
-            <Specs /><ScrollButton />
-            <Cards />
-            <Footer bg={NavImg}/>
-        </div>
-    )
+      <div>
+        <Navbar bg={NavImg} />
+        <ImageSlider scrollStepInPx="1" delayInMs="16.66" />
+        <Specs />
+        <ScrollButton />
+        <Cards />
+        <Footer bg={NavImg} />
+      </div>
+    );
+  }
 }
-
-export default Home
+export default Home;
