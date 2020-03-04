2c9bf41201837687baec7a3d8dd1b4d6c27875c6
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-03-04 16:00:42 +0200

    onSubmitRegisterFunction

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,65 +1,106 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
+const NavBar = props => {
+  const NavStyle = {
+    width: "100%",
+    flexGrow: "1",
+    backgroundImage: `url(${props.bg})`
+  };
+  return (
+    <nav className="navbar navbar-dark" style={NavStyle}>
+      <div>
+        <Link to="/" className="navbar-brand" style={{ color: "black" }}>
+          <i className="fas fa-ticket-alt" /> TicketBox
+        </Link>
+        <small
+          className="navbar-brand"
+          style={{ fontSize: "12px", color: "black" }}
+        >
+          for E-Booking Services
+        </small>
+      </div>
 
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
 
+      <div
+        style={{
+          float: "right",
+          display: "flex",
+          alignItems: "stretch",
+          justifyContent: "space-around"
+        }}
+      >
+        <Link to="/login" className="navbar-brand" style={{ color: "black" }}>
+          <i className="far fa-user" /> Login
+        </Link>
+        <Link
+          to="/register"
+          className="navbar-toggler"
+          style={{
+            float: "left",
+            backgroundColor: "green",
+            color: "white",
+            paddingTop: "9px"
+          }}
+        >
+          <i className="fas fa-plus" /> Join for free
+        </Link>
 
-const NavBar = (props) =>{
-      
-    
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+          style={{}}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
 
-    const NavStyle={
-      width:'100%',flexGrow:'1',backgroundImage:`url(${props.bg})`
-    }
-    return (
-           <nav className="navbar navbar-dark" style={NavStyle}>
-  <div>
-  <Link to='/' className="navbar-brand" style={{color:'black'}} ><i className="fas fa-ticket-alt"/> TicketBox</Link>
-  <small className="navbar-brand"style={{fontSize:'12px',color:'black'}} >for E-Booking Services</small>
-  </div>
- 
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              {" "}
+              <i className="fas fa-home" /> Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/admin" className="nav-link">
+              {" "}
+              <i className="fas fa-home" /> Admin
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/About" className="nav-link">
+              <i className="fas fa-info-circle" /> About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              {" "}
+              <i className="far fa-user" /> Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-link">
+              <i className="fas fa-sign-in-alt" /> signup
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              {" "}
+              <i className="fas fa-sign-out-alt" /> Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-<label className="switch">
-  <input type="checkbox"/>
-  <span className="slider round"></span>
-</label>
-
-
-  <div style={{float:'right',display:'flex', alignItems:'stretch',justifyContent:'space-around'}}>
-  <Link to='/login'className="navbar-brand" style={{color:'black'}} ><i className="far fa-user"/> Login</Link>
-  <Link to='/register' className="navbar-toggler" style={{float:'left',backgroundColor:'green',color:'white',paddingTop:'9px'}}>
-  <i className="fas fa-plus"/> Join for free</Link>
-  
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" style={{}}>
-    <span className="navbar-toggler-icon"></span>
-  </button></div>
-
- 
-  <div className="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link to='/' className="nav-link" > <i className="fas fa-home"/> Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link to='/admin' className="nav-link" > <i className="fas fa-home"/> Admin</Link>
-      </li>
-      <li className="nav-item">
-        <Link to='/About' className="nav-link" ><i className="fas fa-info-circle" />   About</Link>
-      </li>
-      <li className="nav-item">
-        <Link to='/login' className="nav-link" > <i className="far fa-user"/> Login</Link>
-      </li>
-      <li className="nav-item">
-        <Link to='/register' className="nav-link" ><i className="fas fa-sign-in-alt"/> signup</Link>
-      </li>
-      <li className="nav-item">
-        <Link to='/login' className="nav-link" > <i className="fas fa-sign-out-alt"/> Logout</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-        )
-    
-}
-
-export default NavBar
+export default NavBar;
--- src/components/layout/SignUp.js
+++ src/components/layout/SignUp.js
@@ -1,50 +1,113 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Signup = () => {
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value }); //e.target.name means whatever name we have take whatever value
+  }
+  onSubmit(e) {
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
+  }
+  render() {
     return (
-        <div className="container">
-          <br />
-        <form>
-        <div className="form-group">
-      <h1 className="flex-center">Create your new account</h1>
-    <label>First Name</label>
-    <input type="text" className="form-control" id="exampleInput"  placeholder="FirstName" style={{width:'40%'}} required/>
-    
-  </div>
-  <div className="form-group">
-      
-    <label>Last Name</label>
-    <input type="text" className="form-control" id="exampleInput"  placeholder="LastName" style={{width:'40%'}} required/>
-    
-  </div>
-  <div className="form-group">
-      
-    <label>Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1"  placeholder="Email" style={{width:'50%'}} required/>
-    
-  </div>
-  <div className="form-group">
-    <label >Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" style={{width:'50%'}} required/>
-  </div>
-  <div className="form-group">
-    <label >reEnter Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" style={{width:'50%'}} required/>
-  </div>
-  <div className="form-group">
-    <label>Enter PhoneNumber</label>
-    <input type="number" className="form-control" id="exampleInputNumber" placeholder="PhoneNumber" style={{width:'50%'}} required/>
-  </div>
-  <div className="form-group form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" required/>
-    <label className="form-check-label" >by clicking her you are accepting our</label><Link to='/terms-and-conditions'> Terms and Conditions.</Link>
-  </div>
-  <button type="submit" className="btn btn-primary">Signup</button>
-  <small>   Do you have an account? You can login by clicking</small><Link to='/login'> here</Link>
-</form>
-<br />
-</div>
-    )}
+      <div className="container">
+        <br />
+        <form onSubmit={onsubmit}>
+          <div className="form-group">
+            <h1 className="flex-center">Create your new account</h1>
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInput"
+              placeholder="FullName"
+              style={{ width: "50%" }}
+              required
+              name="name"
+              value={this.state.name}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Email"
+              style={{ width: "50%" }}
+              required
+              name="email"
+              value={this.state.email}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              style={{ width: "50%" }}
+              required
+              name="password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>reEnter Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              style={{ width: "50%" }}
+              required
+              name="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+              required
+            />
+            <label className="form-check-label">
+              by clicking her you are accepting our
+            </label>
+            <Link to="/terms-and-conditions"> Terms and Conditions.</Link>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Signup
+          </button>
+          <small> Do you have an account? You can login by clicking</small>
+          <Link to="/login"> here</Link>
+        </form>
+        <br />
+      </div>
+    );
+  }
+}
 
-export default Signup
+export default Signup;
