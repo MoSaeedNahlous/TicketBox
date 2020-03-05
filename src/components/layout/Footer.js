ffdf5ab23d60f35546c85f7ba6e60c34b2c76500
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-03-05 14:32:44 +0200

    AboutUsOurTeam

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,81 +1,137 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Footer = (props) => {
-  const FooterStyle={backgroundColor:'#494E53' ,color :'white' ,width:'100%',backgroundImage:`url(${props.bg})`}
-    return (
-        
-        
-<footer className="  page-footer font-small fluid full-width bg-dark-gray" style={FooterStyle}>
- 
-<div className="container-fluid">
-      <div className="row mt3 full-width">
-        <div className="col-md-4" style={{color:'white'}}>
-          <h5 className="font-weight-bold text-uppercase mt3  " style={{color:'white',marginLeft:'5%',marginTop:'3.2%'}}>Company Details</h5>
-          <ul className="list-unstyled">
-            <li><Link to='/aboutUs' style={{color:'white'}}><i className="fas fa-hand-point-right"/> About us</Link></li>
-            <li><Link to='/contactUs' style={{color:'white'}}><i className="fas fa-hand-point-right"/ > Contact us</Link></li>
-            <li><Link to='/partnerAffiliation' style={{color:'white'}}><i className="fas fa-hand-point-right"/> Partnership/Affiliation</Link></li>
-            <li><Link to='/termsAndConditions' style={{color:'white'}}><i className="fas fa-hand-point-right"/> Terms and Conditions</Link></li>
-            <li><Link to='/cookiePolicy' style={{color:'white'}}><i className="fas fa-hand-point-right"/> Cookie Policy</Link></li>
-          </ul>
+const Footer = props => {
+  const FooterStyle = {
+    backgroundColor: "#494E53",
+    color: "white",
+    width: "100%",
+    backgroundImage: `url(${props.bg})`
+  };
+  return (
+    <footer
+      className="  page-footer font-small fluid full-width bg-dark-gray"
+      style={FooterStyle}
+    >
+      <div className="container-fluid">
+        <div className="row mt3 full-width">
+          <div className="col-md-4" style={{ color: "white" }}>
+            <h5
+              className="font-weight-bold text-uppercase mt3  "
+              style={{ color: "white", marginLeft: "5%", marginTop: "3.2%" }}
+            >
+              Company Details
+            </h5>
+            <ul className="list-unstyled">
+              <li>
+                <Link to="/aboutUs" style={{ color: "white" }}>
+                  <i className="fas fa-hand-point-right" /> Our Team
+                </Link>
+              </li>
+              <li>
+                <Link to="/contactUs" style={{ color: "white" }}>
+                  <i className="fas fa-hand-point-right" /> Contact us
+                </Link>
+              </li>
+              <li>
+                <Link to="/partnerAffiliation" style={{ color: "white" }}>
+                  <i className="fas fa-hand-point-right" />{" "}
+                  Partnership/Affiliation
+                </Link>
+              </li>
+              <li>
+                <Link to="/termsAndConditions" style={{ color: "white" }}>
+                  <i className="fas fa-hand-point-right" /> Terms and Conditions
+                </Link>
+              </li>
+              <li>
+                <Link to="/cookiePolicy" style={{ color: "white" }}>
+                  <i className="fas fa-hand-point-right" /> Cookie Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <h5
+              className="font-weight-bold text-uppercase mt-3 mb-4"
+              style={{ color: "white" }}
+            >
+              Download our App
+            </h5>
+            <ul className="list-unstyled">
+              <li>
+                <button className="btn btn-outline-secondary" disabled>
+                  <i className="fab fa-google-play fa-2x" /> Coming soon{" "}
+                </button>
+              </li>
+              <br />
+              <li>
+                <button
+                  className="btn btn-outline-secondary "
+                  style={{ paddingLeft: "19px" }}
+                  disabled
+                >
+                  <i
+                    className="fab fa-apple fa-2x"
+                    style={{ marginLeftLeft: "10px" }}
+                  />{" "}
+                  Coming soon
+                </button>
+              </li>
+            </ul>
+            <ul className="list-unstyled"></ul>
+          </div>
+          <div className="col-md-4">
+            <h5
+              className="font-weight-bold text-uppercase mt-3 mb-4"
+              style={{ color: "white" }}
+            >
+              Follow us
+            </h5>
+            <div className="row">
+              <Link to="/" style={{ margin: "3%" }}>
+                <i
+                  className="fab fa-facebook-square fa-2x"
+                  style={{ color: "#4267B2" }}
+                />
+              </Link>
+
+              <Link to="/" style={{ margin: "3%" }}>
+                <i
+                  className="fab fa-twitter-square fa-2x"
+                  style={{ color: "#1DA1F2" }}
+                />
+              </Link>
+
+              <Link to="/" style={{ margin: "3%" }}>
+                <i
+                  className="fab fa-instagram-square fa-2x"
+                  style={{ color: "#CF4468" }}
+                />
+              </Link>
+
+              <Link to="/" style={{ margin: "3%" }}>
+                <i
+                  className="fab fa-telegram fa-2x"
+                  style={{ color: "#35ADE1" }}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="col-md-4">
-        <h5 className="font-weight-bold text-uppercase mt-3 mb-4" style={{color:'white'}}>Download our App</h5>
-        <ul className="list-unstyled">
-          <li>
-        <button className="btn btn-outline-secondary" disabled><i className="fab fa-google-play fa-2x"/> Coming soon </button>
-          </li>
-          <br />
-          <li>
-        <button className="btn btn-outline-secondary " style={{paddingLeft:'19px'}} disabled><i className="fab fa-apple fa-2x" style={{marginLeftLeft:'10px'}}/>  Coming soon</button>
-        </li>
-        </ul>
-         <ul className="list-unstyled">
-          
-        </ul>
-        </div>
-        <div className="col-md-4">
-        <h5 className="font-weight-bold text-uppercase mt-3 mb-4" style={{color:'white'}}>Follow us</h5>
+
+        <hr />
         <div className="row">
-         
-            <Link to="/" style={{margin:'3%'}} ><i className="fab fa-facebook-square fa-2x" style={{color:'#4267B2'}}/></Link>
-          
-         
-            <Link to="/"  style={{margin:'3%'}} ><i className="fab fa-twitter-square fa-2x" style={{color:'#1DA1F2'}} /></Link>
-          
-         
-            <Link to="/"  style={{margin:'3%'}}><i className="fab fa-instagram-square fa-2x" style={{color:'#CF4468'}}/></Link>
-          
-         
-            <Link to="/"  style={{margin:'3%'}} ><i className="fab fa-telegram fa-2x" style={{color:'#35ADE1'}}/></Link>
-          
+          <div
+            className="footer-copyright text-center py-1 col-md-6 "
+            style={{ color: "white" }}
+          >
+            © 2020 Copyright
+          </div>
         </div>
-        </div>
-
       </div>
+    </footer>
+  );
+};
 
-
-       
-
-      
-  <hr />
-      <div className="row">
-
-        <div className="footer-copyright text-center py-1 col-md-6 " style={{color:'white'}}>
-           © 2020 Copyright
-    
-  </div>
-  
-      </div>
-  </div>
-  
-  
-  
-
-</footer>
-
-    )
-}
-
-export default Footer
+export default Footer;
--- src/components/layout/WorkerCard.js
+++ src/components/layout/WorkerCard.js
@@ -1,58 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const WorkerCard = () => {
-    return (
-        
-        <div className="container">
-            <br />
-            <h1 style={{textAlign:'center'}}>About us</h1>
-                                <div className="row">
-                                    <div className="col-md-4">
-                                    <div className="flip-card">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}}/>
-                        </div>
-                        <div className="flip-card-back">
-                        <h1>Youssef Kafa</h1>
-                        <p>Back End Developer</p>
-                        <p>skilled with Spring Framework - "His Nickname is Lastok!"</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                <div className="flip-card">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}}/>
-                        </div>
-                        <div className="flip-card-back">
-                        <h1>Mohammad Saeed Nahlous</h1>
-                        <p>Front End Developer</p>
-                        <p>Hardworker with alot of creativity - "he is laughing all the time!"</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                <div className="flip-card">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                        <img src="img_avatar.png" alt="Avatar" style={{width:'300px',height:'300px'}}/>
-                        </div>
-                        <div className="flip-card-back">
-                        <h1>Abdullah Jeero</h1>
-                        <p>Android Full Stack</p>
-                        <p>Skilled with Android Studio - "He is always Late for everything!"</p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div> 
-            <br />
-            </div> 
-    )
-}
+  return (
+    <div className="container">
+      <br />
+      <h1 style={{ textAlign: "center" }}>Our Team</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  src="img_avatar.png"
+                  alt="Avatar"
+                  style={{ width: "300px", height: "300px" }}
+                />
+              </div>
+              <div className="flip-card-back">
+                <h1>Youssef Kafa</h1>
+                <p>Back End Developer</p>
+                <p>skilled with Spring Framework - "His Nickname is Lastok!"</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  src="img_avatar.png"
+                  alt="Avatar"
+                  style={{ width: "300px", height: "300px" }}
+                />
+              </div>
+              <div className="flip-card-back">
+                <h1>Mohammad Saeed Nahlous</h1>
+                <p>Front End Developer</p>
+                <p>
+                  Hardworker with alot of creativity - "he is laughing all the
+                  time!"
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  src="img_avatar.png"
+                  alt="Avatar"
+                  style={{ width: "300px", height: "300px" }}
+                />
+              </div>
+              <div className="flip-card-back">
+                <h1>Abdullah Jeero</h1>
+                <p>Android Full Stack</p>
+                <p>
+                  Skilled with Android Studio - "He is always Late for
+                  everything!"
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+    </div>
+  );
+};
 
-export default WorkerCard
+export default WorkerCard;
