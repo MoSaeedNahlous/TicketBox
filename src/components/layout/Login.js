d69caf08d65875b8d2c28837fe815bb9a4b21d90
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-03-09 13:15:32 +0200

    loginLinkAdded

--- src/components/layout/Login.js
+++ src/components/layout/Login.js
@@ -1,60 +1,90 @@
-import React from 'react'
-import {Link, useHistory} from 'react-router-dom'
-import { useState } from 'react';
-import axios from 'axios'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-
-
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const history = useHistory();
 
-  const HandleSubmit = (e) => {
+  const HandleSubmit = e => {
     e.preventDefault();
-    const user={"email":email,"password":password}
+    const user = { email: email, password: password };
     console.log(user);
-// todo:add URL for posting login requests
-    axios.post("insert URL Here!!!",user).then((response) => {
-      console.log(response);
-      alert("Success!! welcome to TicketBox!")
-      history.push('/Home')
-    })
-    .catch( (err) => {
-      console.log(err);
-      alert(err);
-    });
-  }
-    return (
-        <div className="container">
-          <br />
-        <form onSubmit={HandleSubmit}>
-  <div className="form-group">
-      <h1 className="flex-center">Login to your account</h1>
-    <label >Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style={{width:'50%'}} placeholder="Enter email" required
-    onChange={(e)=>{setEmail(e.target.value)}}/>
-  </div>
-  <div className="form-group">
-    <label>Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" style={{width:'50%'}} required
-    onChange={(e)=>{setPassword(e.target.value)}}/>
-  </div>
-  <div className="form-group form-check">
-    <input type="checkbox" className="form-check-input"  id="exampleCheck1"/>
-    <label className="form-check-label" >Save my password</label>
-  </div>
-  <button type="submit" onSubmit={HandleSubmit} className="btn btn-primary">Login</button>
-  <br/>
-  <small>   Forget your password?Click</small><Link to='/'> here</Link>
-  <br/>
-  <small>   Don't have an account? You can SignUp by clicking</small><Link to='/register'> here</Link>
-</form><br /></div>
+    // todo:add URL for posting login requests
+    axios
+      .post("http://localhost:9090/api/users/login", user)
+      .then(response => {
+        console.log(response);
+        alert("Success!! welcome to TicketBox!");
+        history.push("/Home");
+      })
+      .catch(err => {
+        console.log(err);
+        alert(err);
+      });
+  };
+  return (
+    <div className="container">
+      <br />
+      <form onSubmit={HandleSubmit}>
+        <div className="form-group">
+          <h1 className="flex-center">Login to your account</h1>
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            style={{ width: "50%" }}
+            placeholder="Enter email"
+            required
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            style={{ width: "50%" }}
+            required
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label">Save my password</label>
+        </div>
+        <button
+          type="submit"
+          onSubmit={HandleSubmit}
+          className="btn btn-primary"
+        >
+          Login
+        </button>
+        <br />
+        <small> Forget your password?Click</small>
+        <Link to="/"> here</Link>
+        <br />
+        <small> Don't have an account? You can SignUp by clicking</small>
+        <Link to="/register"> here</Link>
+      </form>
+      <br />
+    </div>
+  );
+};
 
-    )}
-
-export default Login
+export default Login;
